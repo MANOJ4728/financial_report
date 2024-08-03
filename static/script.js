87e9c29a6ec63a79fdf6c2004468b10cbb6296c9
MANOJ4728 <56445559+MANOJ4728@users.noreply.github.com>
2024-08-03 18:01:01 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,45 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var ctx = document.getElementById('donutChart').getContext('2d');
+    
+    var donutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Total Expenditure', 'Profit'],
+            datasets: [{
+                label: 'OVERVIEEW',
+                data: [139493.05, 51895.79],
+                borderWidth: 1
+            }]
+        },
+       
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var ctx = document.getElementById('myHorizontalBarChart').getContext('2d');
+
+    var myHorizontalBarChart = new Chart(ctx, {
+        type: 'bar', // Chart type
+        data: {
+            labels: ['Actual Revenue', 'Budgeted Revenue', 'Previous Period Revenue'],
+            datasets: [{
+                label: 'Revenue Growth',
+                data: [191388.84, 168224.66, 223980.90], // Corrected data array
+                backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'], // Added color for the third bar
+                borderColor: ['rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'], // Added color for the third bar
+                borderWidth: 1
+            }]
+        },
+        options: {
+            indexAxis: 'y', // This makes the bars horizontal
+            scales: {
+                x: {
+                    beginAtZero: true // Ensures that the x-axis starts at 0
+                },
+                y: {
+                    beginAtZero: true // Ensures that the y-axis starts at 0
+                }
+            }
+        }
+    });
+});
